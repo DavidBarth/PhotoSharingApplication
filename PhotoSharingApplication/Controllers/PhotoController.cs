9adaa2f9f6e6eb4fbed4f3a58b18f1298e0319d0
David Barth <davidbarth80@gmail.com>
2016-11-08 15:08:28 +0000

    Caching of photo contoller index and getImage actions

--- PhotoSharingApplication/Controllers/PhotoController.cs
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using System.Globalization;
 using PhotoSharingApplication.Models;
 
@@ -26,6 +27,7 @@ namespace PhotoSharingApplication.Controllers
         //
         // GET: /Photo/
 
+        [OutputCache(Duration=600, Location=OutputCacheLocation.Server, VaryByParam="none")]
         public ActionResult Index()
         {
             return View("Index");
@@ -121,6 +123,8 @@ namespace PhotoSharingApplication.Controllers
             return RedirectToAction("Index");
         }
 
+
+        [OutputCache(Duration = 600, Location = OutputCacheLocation.Server, VaryByParam = "id")]
         public FileContentResult GetImage(int id)
         {
             Photo photo = context.FindPhotoById(id);
